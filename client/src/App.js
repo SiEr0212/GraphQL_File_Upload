595f21c8a2c15bc3d70a90926a1a216d7c3cfac6
SiEr0212 <simonertel@gmx.de>
2021-02-27 19:21:50 +0100

    adding upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { introspectionQuery } from 'graphql';
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
